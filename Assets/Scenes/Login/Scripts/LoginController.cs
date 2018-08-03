98ac3c9ff8f6002f76d0bb36dada31b0cc6a8ba5
YizhouCodes <contact.yizhou@gmail.com>
2018-08-03 21:19:11 +0300

    Simple validation and login data persistance added
    Simple non-null validation added for login fields. In addition, login data used previously persists in the corresponding fields for future usage.

--- Assets/Scenes/Login/Scripts/LoginController.cs
+++ Assets/Scenes/Login/Scripts/LoginController.cs
@@ -14,10 +14,16 @@ public class LoginController : MonoBehaviour {
     public InputField streamIdField;
     public InputField apiKeyField;
 
+    bool displayMessage = false;
+
 
 	// Use this for initialization
 	void Start () {
-		
+        if (PlayerPrefs.GetString("username") != "") {
+            usernameField.text = PlayerPrefs.GetString("username");
+            streamIdField.text = PlayerPrefs.GetString("streamId");
+            apiKeyField.text = PlayerPrefs.GetString("apiKey");
+        }
 	}
 	
 	// Update is called once per frame
@@ -25,15 +31,26 @@ public class LoginController : MonoBehaviour {
 		
 	}
 
+    void OnGUI()
+    {
+        if (displayMessage) {
+            GUI.Label(new Rect(Screen.width / 2, Screen.height / 3, 200f, 200f), "All the fields are mandatory.");
+        }
+    }
+
     public void Submit() {
-        username = usernameField.text;
-        streamId = streamIdField.text;
-        apiKey = apiKeyField.text;
+        if (usernameField.text != "" && streamIdField.text != "" && apiKeyField.text != "") {
+            username = usernameField.text;
+            streamId = streamIdField.text;
+            apiKey = apiKeyField.text;
 
-        PlayerPrefs.SetString("username", username);
-        PlayerPrefs.SetString("streamId", streamId);
-        PlayerPrefs.SetString("apiKey", apiKey);
+            PlayerPrefs.SetString("username", username);
+            PlayerPrefs.SetString("streamId", streamId);
+            PlayerPrefs.SetString("apiKey", apiKey);
 
-        SceneManager.LoadScene("Scenes/World/World");
+            SceneManager.LoadScene("Scenes/World/World");
+        } else {
+            displayMessage = true;
+        }
     }
 }
