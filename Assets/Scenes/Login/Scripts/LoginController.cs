866dc054192d2457a489e4d80ddcc11bdbd00782
YizhouCodes <contact.yizhou@gmail.com>
2018-08-03 16:36:43 +0300

    Login Scene data persistance added
    Login Scene now saves player data, and automatically switches to the World Scene.

--- Assets/Scenes/Login/Scripts/LoginController.cs
+++ Assets/Scenes/Login/Scripts/LoginController.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LoginController : MonoBehaviour {
-
+    
     private string username;
     private string streamId;
     private string apiKey;
 
-    private string usernameString = string.Empty;
-    private string streamIdString = string.Empty;
-    private string apiKeyString = string.Empty;
+    public InputField usernameField;
+    public InputField streamIdField;
+    public InputField apiKeyField;
 
 
 	// Use this for initialization
@@ -23,17 +25,15 @@ public class LoginController : MonoBehaviour {
 		
 	}
 
-    //void OnGUI()
-    //{
-    //    GUI.Window(0, windowRect, WindowFunction, "Login");
-    //}
+    public void Submit() {
+        username = usernameField.text;
+        streamId = streamIdField.text;
+        apiKey = apiKeyField.text;
 
-    //void WindowFunction(int windowId) {
-    //    usernameString = GUI.TextField(new Rect(Screen.width / 3, 2 * Screen.height / 5, Screen.width / 3, Screen.height / 10), usernameString, 15);
-    //    streamIdString = GUI.TextField(new Rect(Screen.width / 3, 2 * Screen.height / 5, Screen.width / 3, Screen.height / 10), streamIdString, 30);
-    //    apiKeyString = GUI.TextField(new Rect(Screen.width / 3, 2 * Screen.height / 5, Screen.width / 3, Screen.height / 10), apiKeyString, 30);
-    //    ethereumString = GUI.TextField(new Rect(Screen.width / 3, 2 * Screen.height / 5, Screen.width / 3, Screen.height / 10), ethereumString, 30);
+        PlayerPrefs.SetString("username", username);
+        PlayerPrefs.SetString("streamId", streamId);
+        PlayerPrefs.SetString("apiKey", apiKey);
 
-    //    GUI.Label(new Rect(Screen.width / 3, 35 * Screen.height / 100, Screen.width / 5, Screen.height / 18), "Username");
-    //}
+        SceneManager.LoadScene("Scenes/World/World");
+    }
 }
